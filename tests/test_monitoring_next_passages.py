2d26ac078dd00b8237c6bf52cd2722cb16f81739
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2025-10-30 15:07:07 +0100

    First dockerfile and cleaning.

--- tests/test_monitoring_next_passages.py
+++ tests/test_monitoring_next_passages.py
@@ -5,30 +5,41 @@ from transportation_monitoring.extract_next_passages import *
 
 
 @pytest.mark.parametrize("monitoring_ref", [
-    "STIF:StopPoint:Q:463158:",
+    "STIF:StopPoint:Q:41855:",
     "STIF:StopPoint:Q:12406:",
 ])
 def test_next_passages(api_key,monitoring_ref):
     stops = idfm_next_passages(api_key, monitoring_ref)
     res = extract_next_passages(stops)
     print_passages_table(res)
-    print(res)
+    #print(res)
 
 
-def test_json_stops_explore():
-    json_data = jload("arrets-lignes.json")
+@pytest.mark.parametrize(("city", "stop_names"), [
+    ("Clamart", ["Cimeti\u00e8re", "Cimeti\u00e8re de Clamart"]),
+    ("Ch\u00e2tillon", ["Division Leclerc"]),
+    ("Fontenay-aux-Roses", ["Division Leclerc"]),
+])
+def test_json_stops_explore(city, stop_names):
+    json_data = jload("transportation_monitoring/arrets-lignes.json")
+    matching_stops = []
     for stop in json_data:
         commune:str = get(stop, "nom_commune")
         name:str = get(stop, "stop_name")
         stop_id:str = get(stop, "stop_id")
         id:str = get(stop, "id")
-        if commune == "Clamart" and name == "Cimeti\u00e8re":
-            print(stop)
+        if commune == city and name in stop_names:
+            matching_stops.append(stop)
+    assert len(matching_stops)>0
+
+    for stop in matching_stops:
+        print(stop)
+
 
 @pytest.mark.parametrize("wanted_line_name", [
     "189", "190",
 ])
-def test_json_stops_explore(wanted_line_name:str):
+def test_json_lines_explore(wanted_line_name:str):
     json_data = jload("transportation_monitoring/referentiel-des-lignes.json")
     for line in json_data:
         short_name:str = get(line, "shortname_line")

1e768d9c11947e99f11d21d33ccd81e57d73c330
guillaumepichon <guillaume.pichon@synchrotron-soleil.fr>
2025-11-03 16:55:51 +0100

    First docs and project file

--- transportation_monitoring/__init__.py
+++ transportation_monitoring/__init__.py
@@ -1,6 +1,8 @@
 from transportation_monitoring.data_explorer import yload
 from transportation_monitoring.extract_next_passages import load_ref_lines
 
+__version__ = "0.1.0"
+
 SECRETS_FILE = "secrets.yaml"
 MONITORING_FILE = "stops_monitoring.yaml"
 secrets = yload(SECRETS_FILE)
